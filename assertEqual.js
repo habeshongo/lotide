812558a3e05a82e12e630eabf5653ff796461f31
Ashton Ferguson <doctorarferguson@gmail.com>
2023-01-28 01:16:30 -0400

    assertEqual function to compare two variables.

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed✅: ${actual} === ${expected}.`);
+  } else {
+    console.log(`Assertion Failed🚫: ${actual} !== ${expected}.`);
+  }
+};
+
+//TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
