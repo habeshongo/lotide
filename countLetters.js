ad72ef794f739ac57487866fdeb962c92293164e
Ashton Ferguson <doctorarferguson@gmail.com>
2023-02-05 01:20:37 -0400

    Function that counts letters in a sentence and returns tha answer as an object.

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed✅: ${actual} === ${expected}.`);
+  } else {
+    console.log(`Assertion Failed🚫: ${actual} !== ${expected}.`);
+  }
+};
+// The function should take in a sentence (as a string) and then return
+//a count of each of the letters in that sentence.
+//Need to return an object.
+// For example, countLetters('LHL') should return results indicating that L appears twice, and H once.
+
+const countLetters = function (str) {
+  let ans = {};
+  for (const character of str) {
+    //console.log(character);
+    if (ans.hasOwnProperty(character)) {
+      ans[character] += 1;
+    } else {
+      ans[character] = 1;
+    }
+  }
+  console.log(ans);
+};
+
+countLetters("LHL");
