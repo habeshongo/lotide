b6ffe1f055f2ae54b1c9659f33e808cf8ea6e759
Ashton Ferguson <doctorarferguson@gmail.com>
2023-02-05 01:32:21 -0400

    Iterating through an object to find the corresponding keys for a given value.

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed✅: ${actual} === ${expected}.`);
+  } else {
+    console.log(`Assertion Failed🚫: ${actual} !== ${expected}.`);
+  }
+};
+
+const findKeyByValue = function (obj, val) {
+  for (const property in obj) {
+    if (obj[property] === val) {
+      return property;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
