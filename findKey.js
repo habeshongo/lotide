93bcc2d714ba5224ed756f07454e1d252c6853f7
Ashton Ferguson <doctorarferguson@gmail.com>
2023-02-06 00:05:38 -0400

    Using a callback function to Find the key in an object when the value is given.

---
+++ findKey.js
@@ -0,0 +1,37 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed✅: ${actual} === ${expected}.`);
+  } else {
+    console.log(`Assertion Failed🚫: ${actual} !== ${expected}.`);
+  }
+};
+
+// Implement the function findKey which takes in an object and a callback.
+// It should scan the object and return the first key for which the callback returns a truthy value.
+// If no key is found, then it should return undefined.
+
+const findKey = function (obj, callback) {
+  let ans = "";
+  for (const key in obj) {
+    let output = callback(obj[key]);
+    if (output) {
+      ans = key;
+      break;
+    }
+  }
+  return ans;
+};
+
+let result1 = findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  },
+  (x) => x.stars === 2
+); // => "noma"
+
+console.log(result1);
