99177b9eb1c2a1fcc63f7e894bebc2ee180504e5
Ashton Ferguson <doctorarferguson@gmail.com>
2023-02-06 00:01:47 -0400

    Slicing an array with a callback function

---
+++ takeUntil.js
@@ -0,0 +1,50 @@
+// It will take in two parameters as well:
+
+// The array to work with
+// The callback (which Lodash calls "predicate")
+
+// The function will return a "slice of the array with elements taken from the beginning."
+// It should keep going until the callback/predicate returns a truthy value.
+
+// To keep things simple, the callback should only be provided one value: The item in the array.
+// Instruction
+// Implement takeUntil which will keep collecting items from a provided array until the callback
+// provided returns a truthy value.
+
+const takeUntil = function (array, callback) {
+  // ...
+
+  let output = [];
+  array.map((e, i) => {
+    let result = callback(e);
+    // console.log(result);
+    if (result) {
+      output = array.slice(0, i);
+    }
+  });
+  return output;
+};
+
+// const callback = function (x) {
+//   return x < 0;
+// };
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, (x) => x < 0);
+console.log(results1);
+
+console.log("---");
+
+// const data2 = [
+//   "I've",
+//   "been",
+//   "to",
+//   "Hollywood",
+//   ",",
+//   "I've",
+//   "been",
+//   "to",
+//   "Redwood",
+// ];
+// const results2 = takeUntil(data2, (x) => x === ",");
+// console.log(results2);
