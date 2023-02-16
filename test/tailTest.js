eba37289d8061c607b060dde005803364370b280
Ashton Ferguson <doctorarferguson@gmail.com>
2023-02-15 22:05:02 -0400

    Refactoring with Mocha and Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,16 @@
 const tail = require("../tail")
 const assertEqual = require("../assertEqual")
 
+const assert = require('chai').assert;
+describe("#tail", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(tail([1, 2, 3]), 1);
+  });
+});
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result[0], "Lighthouse"); // => will always fail!
-assertEqual(result[1], "Labs"); // => will always fail!
-assertEqual(result.length, 2); // => will always fail!
+
+
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result[0], "Lighthouse"); // => will always fail!
+// assertEqual(result[1], "Labs"); // => will always fail!
+// assertEqual(result.length, 2); // => will always fail!
