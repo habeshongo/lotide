5ecece119366700e6d4aaffcebf93c631b6c7801
Ashton Ferguson <doctorarferguson@gmail.com>
2023-03-08 01:30:24 -0400

    Update to remove commented out code.

--- letterPositions.js
+++ letterPositions.js
@@ -1,19 +1,3 @@
-// const eqArrays = function (arr1, arr2) {
-//   if (arr1.length !== arr2.length) {
-//     return false;
-//   }
-//   for (let i = 0; i < arr1.length; i++) {
-//     let ele1 = arr1[i];
-//     let ele2 = arr2[i];
-//     //console.log(ele1, ele2);
-//     if (ele1 !== ele2) {
-//       //console.log(ele1, ele2);
-//       return false;
-//     }
-//   }
-//   return true;
-// };
-
 const letterPositions = function (sentence) {
   const results = {};
   // logic to update results here
