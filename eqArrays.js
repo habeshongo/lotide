8a0a34b56e9e04c65a835254c3961d7bf633c88f
Ashton Ferguson <doctorarferguson@gmail.com>
2023-03-08 23:22:50 -0400

    Update to include appropriate comments.

--- eqArrays.js
+++ eqArrays.js
@@ -1,4 +1,4 @@
-
+//Function that takes in 2 arrays and returns true if the arrays are equal and false otherwise.
 const eqArrays = function (arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -13,4 +13,4 @@ const eqArrays = function (arr1, arr2) {
   return true;
 };
 
-module.exports = eqArrays
+module.exports = eqArrays;
