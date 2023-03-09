31dcd827af977e2256d58f9ebb1633a301b44b2d
Ashton Ferguson <doctorarferguson@gmail.com>
2023-03-09 00:02:54 -0400

    Update - Added function description.

--- without.js
+++ without.js
@@ -1,3 +1,6 @@
+/*Function that takes in a source array and an itemsToRemove arrays and returns 
+only the items from the source array that are not in the itemsToRemove array.*/
+
 const without = function (arr1, arr2) {
   let ans = [];
   for (let i = 0; i < arr1.length; i++) {
