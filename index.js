3d7405824f18cf882af454c4fe5dd841629a1259
Ashton Ferguson <doctorarferguson@gmail.com>
2023-03-08 23:59:07 -0400

    Update - Added all functions to be exported.

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
-const head = require('./head')
-const tail = require('./tail')
-const middle = require('./middle')
-const eqArrays = require('./eqArrays')
-const assertEqualTest = require('./assertEqual')
-const assertArraysEqualTest = require('./assertArraysEqual')
+const assertArraysEqual = require("./assertArraysEqual");
+const assertEqual = require("./assertEqual");
+const assertObjectsEqual = require("./assertObjectsEqual");
+const countLetters = require("./countLetters");
+const countOnly = require("./countOnly");
+const eqArrays = require("./eqArrays");
+const eqObjects = require("./eqObjects");
+const findkey = require("./findKey");
+const findKeyByValue = require("./findKeyByValue");
+const flatten = require("./flatten");
+const head = require("./head");
+const letterPositions = require("./letterPositions");
+const map = require("./map");
+const middle = require("./middle");
+const tail = require("./tail");
+const takeUntil = require("./takeUntil");
 
+const assertEqualTest = require("./assertEqual");
+const assertArraysEqualTest = require("./assertArraysEqual");
 
-
+//All functions are exported as an object.
 module.exports = {
-  head,
-  tail,
-  middle,
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
   eqArrays,
+  eqObjects,
+  findkey,
+  findKeyByValue,
+  flatten,
+  head,
+  letterPositions,
+  map,
+  middle,
+  tail,
+  takeUntil,
   assertEqualTest,
-  assertArraysEqualTest
-}
+  assertArraysEqualTest,
+};
