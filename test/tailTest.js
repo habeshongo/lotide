800e20a51062d3a90dfffe585d51e5534b7b697f
Ashton Ferguson <doctorarferguson@gmail.com>
2023-03-08 23:02:52 -0400

    Update test case.

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,16 +1,16 @@
-const tail = require("../tail")
-const assertEqual = require("../assertEqual")
+const tail = require("../tail");
+const assertEqual = require("../assertEqual");
 
-const assert = require('chai').assert;
+const assert = require("chai").assert;
 describe("#tail", () => {
-  it("returns 1 for [1, 2, 3]", () => {
-    assert.strictEqual(tail([1, 2, 3]), 1);
+  it("returns [2,3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
   });
 });
-
-
 
 // const result = tail(["Hello", "Lighthouse", "Labs"]);
 // assertEqual(result[0], "Lighthouse"); // => will always fail!
 // assertEqual(result[1], "Labs"); // => will always fail!
 // assertEqual(result.length, 2); // => will always fail!
+
+//Deep equal checks for every element of the array. Strict equal cannot do an equality check of an array. Deepequal can.
