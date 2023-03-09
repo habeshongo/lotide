1d2747b92529d50dfc7cfa4d10a120d7ce85f017
Ashton Ferguson <doctorarferguson@gmail.com>
2023-03-08 23:10:12 -0400

    Update to break the loop when a condition is met.

--- takeUntil.js
+++ takeUntil.js
@@ -11,15 +11,16 @@
 
 const takeUntil = function (array, callback) {
   let output = [];
-  array.map((e, i) => {
-    let result = callback(e);
-     if (result) {
-      output = array.slice(0, i);
+
+  for (let item of array) {
+    if (!callback(item)) {
+      output.push(item);
+    } else {
+      break;
     }
-  });
+  }
   return output;
 };
-
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, (x) => x < 0);
