c890b6030383c44e6c56729a21d7c3423340ce19
Ashton Ferguson <doctorarferguson@gmail.com>
2023-03-08 23:21:54 -0400

    Update to print an appropriate message after comparing the two arrays.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,7 +1,9 @@
-const eqArrays = require("./eqArrays")
+const eqArrays = require("./eqArrays");
 
+//The eqArrays function takes in 2 arrays and returns true if the arrays are equal and false otherwise.
+//assertArraysEqual prints an appropriate message after comparing the two arrays.
 const assertArraysEqual = function (arr1, arr2) {
   console.log(eqArrays(arr1, arr2));
 };
 
-module.exports = assertArraysEqual
+module.exports = assertArraysEqual;
