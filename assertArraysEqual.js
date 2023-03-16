0fb16d725911e988ba445a02c70f5eee598e19e6
Ashton Ferguson <doctorarferguson@gmail.com>
2023-03-15 23:00:01 -0300

    [Update] - Print a more meaningful message for assertion test.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -3,7 +3,11 @@ const eqArrays = require("./eqArrays");
 //The eqArrays function takes in 2 arrays and returns true if the arrays are equal and false otherwise.
 //assertArraysEqual prints an appropriate message after comparing the two arrays.
 const assertArraysEqual = function (arr1, arr2) {
-  console.log(eqArrays(arr1, arr2));
+  if (eqArrays(arr1, arr2)) {
+    console.log(`Assertion Passed✅: ${actual} === ${expected}.`);
+  } else {
+    console.log(`Assertion Failed🚫: ${actual} !== ${expected}.`);
+  }
 };
 
 module.exports = assertArraysEqual;
