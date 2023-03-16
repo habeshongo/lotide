432d79683c6612bf1c1540539aa8a7ed9879807d
Ashton Ferguson <doctorarferguson@gmail.com>
2023-03-15 22:55:47 -0300

    [Update] - Refactor second if condition.

--- tail.js
+++ tail.js
@@ -1,15 +1,14 @@
 const tail = function (arr) {
   let newArr = [];
   //An array with only one element or an empty array should yield an empty array for its tail
-  if(arr.length === 1 || arr.length === 0){
+  if (arr.length === 1 || arr.length === 0) {
     return newArr;
-  }
-  if (arr.length > 1) {
+  } else {
     for (let i = 1; i < arr.length; i++) {
       newArr.push(arr[i]);
     }
     return newArr;
-  } 
+  }
 };
 
-module.exports = tail
+module.exports = tail;
